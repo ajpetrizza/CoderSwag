569f8b5ec6168e2b643efba3bc3f34608d237f59
ajpetrizza <ajpetrizza@fullsail.edu>
2018-01-05 13:05:35 -0500

    Added collectionViewController and collectionViewCell

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Anthony Petrizza on 1/5/18.
+//  Copyright © 2018 Anthony Petrizza. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
